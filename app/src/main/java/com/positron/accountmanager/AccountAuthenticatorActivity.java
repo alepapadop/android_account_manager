62287e726072030889cde3cf4939f6aef18fc2d9
alepapadop <alepapadop@ymail.com>
2016-03-31 01:33:08 +0300

    Some small fixes.

--- app/src/main/java/com/positron/accountmanager/AccountAuthenticatorActivity.java
+++ app/src/main/java/com/positron/accountmanager/AccountAuthenticatorActivity.java
@@ -4,23 +4,17 @@ import android.accounts.Account;
 import android.accounts.AccountManager;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.View;
-import android.widget.EditText;
+
 
 public class AccountAuthenticatorActivity extends android.accounts.AccountAuthenticatorActivity {
 
-    private String _auth_token_type = null;
-    private Integer _account_request = null;
-    private AccountManager _account_manager = null;
-    private Integer _request = null;
-    private String _username = null;
-    private String _password = null;
-    private String _auth_token = null;
+    private String          _auth_token_type = null;
+    private Integer         _account_request = null;
+    private AccountManager  _account_manager = null;
+    private Integer         _request = null;
+    private String          _username = null;
+    private String          _password = null;
+    private String          _auth_token = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,10 +23,10 @@ public class AccountAuthenticatorActivity extends android.accounts.AccountAuthen
         Bundle bundle = getIntent().getExtras();
 
         if (bundle != null) {
-            _request = bundle.getInt(Constants.ACCOUNT_REQUEST_ORIGIN);
+            _request = bundle.getInt(Constants.ACCOUNT_REQUEST_ORIGIN_KEY);
 
             _auth_token_type = bundle.getString(Constants.ACCOUNT_AUTH_TOKEN_TYPE_KEY);
-            _account_request = bundle.getInt(Constants.ACCOUNT_REQUEST_ORIGIN);
+            _account_request = bundle.getInt(Constants.ACCOUNT_REQUEST_ORIGIN_KEY);
 
         }
 
@@ -48,11 +42,11 @@ public class AccountAuthenticatorActivity extends android.accounts.AccountAuthen
 
         Intent intent = null;
 
-        if (_request == Constants.ACCOUNT_REQUEST_ADD_ACCOUNT) {
+        if (_request.equals(Constants.ACCOUNT_REQUEST_ADD_ACCOUNT)) {
             intent = new Intent(this, SignUp.class);
-        } else if (_request == Constants.ACCOUNT_REQUEST_SIGN_UP) {
+        } else if (_request.equals(Constants.ACCOUNT_REQUEST_SIGN_UP)) {
             intent = new Intent(this, SignUp.class);
-        } else if (_request == Constants.ACCOUNT_REQUEST_SIGN_IN) {
+        } else if (_request.equals(Constants.ACCOUNT_REQUEST_SIGN_IN)) {
             intent = new Intent(this, SignIn.class);
         } else {
             assert true;
@@ -67,19 +61,19 @@ public class AccountAuthenticatorActivity extends android.accounts.AccountAuthen
         // Check which request we're responding to
         if (requestCode == Constants.ACCOUNT_REQUEST_ADD_ACCOUNT) {
             if (resultCode == RESULT_OK) {
-                _username = data.getStringExtra(Constants.ACCOUNT_USERNAME);
-                _password = data.getStringExtra(Constants.ACCOUNT_PASSWORD);
+                _username = data.getStringExtra(Constants.ACCOUNT_USERNAME_KEY);
+                _password = data.getStringExtra(Constants.ACCOUNT_PASSWORD_KEY);
                 _auth_token = data.getStringExtra(Constants.ACCOUNT_AUTH_TOKEN_TYPE_KEY);
 
                 createAccount();
             }
         } else if (requestCode == Constants.ACCOUNT_REQUEST_SIGN_UP) {
             if (resultCode == RESULT_OK) {
-
+                assert true;
             }
         } else if (requestCode == Constants.ACCOUNT_REQUEST_SIGN_IN) {
             if (resultCode == RESULT_OK) {
-
+                assert true;
             }
         } else {
             assert true;
@@ -89,7 +83,6 @@ public class AccountAuthenticatorActivity extends android.accounts.AccountAuthen
     }
 
     private void createAccount() {
-
 
         final Account account = new Account(_username, _auth_token_type);
 
--- app/src/main/java/com/positron/accountmanager/SignUp.java
+++ app/src/main/java/com/positron/accountmanager/SignUp.java
@@ -1,14 +1,11 @@
 package com.positron.accountmanager;
 
-import android.accounts.Account;
-import android.accounts.AccountManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
@@ -22,28 +19,36 @@ public class SignUp extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        if (fab != null) {
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
+            });
+        }
 
 
     }
 
     public void sign_up_commit(View view) {
+        String username = null;
+        String password = null;
 
         EditText username_te = (EditText)findViewById(R.id.sign_up_username);
-        String username = username_te.getText().toString();
+        if (username_te != null) {
+            username = username_te.getText().toString();
+        }
 
         EditText password_te = (EditText)findViewById(R.id.sign_up_password);
-        String password = password_te.getText().toString();
+        if (password_te != null) {
+            password = password_te.getText().toString();
+        }
 
         Intent intent = new Intent();
-        intent.putExtra(Constants.ACCOUNT_USERNAME, username);
-        intent.putExtra(Constants.ACCOUNT_PASSWORD, password);
+        intent.putExtra(Constants.ACCOUNT_USERNAME_KEY, username);
+        intent.putExtra(Constants.ACCOUNT_PASSWORD_KEY, password);
         intent.putExtra(Constants.ACCOUNT_AUTH_TOKEN_KEY, Constants.TMP_AUTH_TOKEN);
         setResult(RESULT_OK, intent);
         finish();
